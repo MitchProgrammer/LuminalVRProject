97fe3631f4f9293517f759114790023975640154
Hazelthenut <Hazel.thenut@outlook.com>
2024-08-21 11:55:37 +1000

    Toggle Added

---
+++ Assets/Scripts/ToggleSnow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class ToggleSnow : MonoBehaviour
+{
+    public Toggle toggle;
+    public ParticleSystem particle;
+
+    void Start()
+    {
+        var emission = particle.emission;
+        // Play the audio source when the scene starts
+        emission.enabled = true;
+
+        // Set the toggle to be on by default
+        toggle.isOn = true;
+
+        // Add a listener to call the ToggleValueChanged method when the toggle's value changes
+        toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(toggle);
+        });
+    }
+
+    // This method is called when the toggle's value changes
+    void ToggleValueChanged(Toggle change)
+    {
+        var emissions = particle.emission;
+        // Play or pause the audio source based on the toggle's value
+        if (change.isOn)
+        {
+            emissions.enabled = true;
+        }
+        else
+        {
+            emissions.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ToggleVoice.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleVoice : MonoBehaviour
+{
+    public Toggle toggle;
+    public AudioSource audioSource;
+    public GameObject subtitleText;
+
+    void Start()
+    {
+        // Play the audio source when the scene starts
+        audioSource.Play();
+        subtitleText.SetActive(true);
+
+        // Set the toggle to be on by default
+        toggle.isOn = true;
+
+        // Add a listener to call the ToggleValueChanged method when the toggle's value changes
+        toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(toggle);
+        });
+    }
+
+    // This method is called when the toggle's value changes
+    void ToggleValueChanged(Toggle change)
+    {
+        // Play or pause the audio source based on the toggle's value
+        if (change.isOn)
+        {
+            audioSource.Play();
+            subtitleText.SetActive(true);
+        }
+        else
+        {
+            audioSource.Pause();
+            subtitleText.SetActive(false);
+        }
+    }
+}
