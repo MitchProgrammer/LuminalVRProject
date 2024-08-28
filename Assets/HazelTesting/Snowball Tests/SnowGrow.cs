6aef6aad0470143a23ff1a9bf4bcdc114bbe9211
Hazelthenut <Hazel.thenut@outlook.com>
2024-08-28 11:20:24 +1000

    snowball and aurora testing part 1

--- Assets/HazelTesting/Snowball Tests/SnowGrow.cs
+++ Assets/HazelTesting/Snowball Tests/SnowGrow.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class SnowGrow : MonoBehaviour
 {
-    public Transform transfirm; 
-    public float size = 1;
-    public Rigidbody rigidBody; 
+    public Transform transfirm;
+    public Rigidbody rigidBody;
+
+    public float size = 0.1f;
+    public float maxSize = 2f;
+    
     public Vector3 velotown; //velocity of the growing obj
     public bool isGrowTime; //if it is on the growing material
     public float growthRate = 0.1f; 
     static public bool holded; //if the snowball isn't being held
+
+    public float shrinkTime = 20f;
+    private bool shrink = false;
 
     private void Start()
     {
@@ -21,20 +27,34 @@ public class SnowGrow : MonoBehaviour
     private void Update()
     {
         velotown = rigidBody.velocity;
-        if (isGrowTime && !holded)
+        if (isGrowTime && !holded && !shrink)
         {
             float velocityMagnitude = velotown.magnitude; // Get the magnitude of the velocity
             if (velocityMagnitude > 0)
             {
                 // Adjust growth rate based on current size to slow growth as size increases
                 float growthAmount = velocityMagnitude * growthRate / size * Time.deltaTime;
-                size += growthAmount;
+                if (size + growthAmount > maxSize)
+                {
+                    size = maxSize;
+                }
+                else
+                {
+                    size += growthAmount;
+                }    
                 transform.localScale = new Vector3(size, size, size); // Apply new size
             }
         }
     }
     #region touchytime
     //is touching ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Snow" && !holded)
+        {
+            EndOfLife();
+        }
+    }
     private void OnCollisionStay(Collision touchy)
     {
         if (touchy.collider.tag == "Snow" && !holded)
@@ -50,5 +70,14 @@ public class SnowGrow : MonoBehaviour
         }
     }
     #endregion
+
+    private void EndOfLife() //testing lifetime feature
+    {
+        float timer = 0;
+        while(timer != shrinkTime)
+        {
+            timer = timer + 1 * Time.deltaTime;
+        }
+    }
 
 }
