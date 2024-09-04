081f49799bab814701fefe2ed96d90bbc9286504
Hazelthenut <Hazel.thenut@outlook.com>
2024-09-04 11:44:56 +1000

    Aurora added + basis for comet
    Do not use comet yet, it will freeze unity

---
+++ Assets/Scripts/CometFly.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometFly : MonoBehaviour
+{
+    public bool goFlyComet;
+    public Transform endLocation;
+    public float speed = 5f;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("c"))
+        {
+            goFlyComet = true;
+        }
+        if (goFlyComet)
+        {
+            StartCoroutine(cometFlytime());
+        }
+    }
+
+    IEnumerator cometFlytime()
+    {
+        float finalx = endLocation.position.x;
+        float finalz = endLocation.position.z;
+        while(goFlyComet)
+        {
+            float newX = Mathf.MoveTowards(transform.position.x, finalx, speed * Time.deltaTime);
+            float newZ = Mathf.MoveTowards(transform.position.z, finalz, speed * Time.deltaTime);
+            transform.position = new Vector3(newX, transform.position.y, newZ);
+        }
+        yield return null;
+    }
+}
