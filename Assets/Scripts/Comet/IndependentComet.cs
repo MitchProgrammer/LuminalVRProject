609d885ad13d32df874aa9cc53f1a9e4f7f6b26b
Hazelthenut <Hazel.thenut@outlook.com>
2024-10-07 12:52:25 +1100

    Independent Comet stuff

---
+++ Assets/Scripts/Comet/IndependentComet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndependentComet : MonoBehaviour
+{
+    public GameObject endPlace;
+    public float moveSpeed;
+    private ParticleSystem cometParticles;
+
+    private void Start()
+    {
+        cometParticles = GetComponent<ParticleSystem>();
+
+        MoveToLocation(); //test
+    }
+
+    public void MoveToLocation()
+    {
+        StartCoroutine(MoveTowardsTarget());
+    }
+
+    private IEnumerator MoveTowardsTarget()
+    {
+        while (Vector3.Distance(transform.position, endPlace.transform.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPlace.transform.position, moveSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        if (cometParticles != null && cometParticles.isPlaying)
+        {
+            cometParticles.Stop();
+        }
+    }
+
+}
